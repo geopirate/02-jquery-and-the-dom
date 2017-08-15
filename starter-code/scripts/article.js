af4385416de9b6446ad4e2f6356044dcb573d2b4
Trey64 <thomasellison.r@gmail.com>
2017-08-15 15:14:08 -0700

    Finished final TODO item

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,33 +3,26 @@
 var articles = [];
 
 function Article (rawDataObj) {
-  $(this).title = rawDataObj.title;
-  $(this).category = rawDataObj.category;
-  $(this).author = rawDataObj.author;
-  $(this).authorUrl = rawDataObj.authorUrl;
-  $(this).publishedOn = rawDataObj.publishedOn;
-  $(this).body = rawDataObj.body;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template.
-  However, in our modules.css stylesheet, we gave all elements
-  with a class of template a display of none. Let's make
-  sure we're not accidentally hiding our cloned article! */
   $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest
-  of the current template clone with properties from this particular Article instance.
-  We need to fill in:
-    1. author name,
-    2. author url,
-    3. article title,
-    4. article body, and
-    5. publication date. */
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.append(this.body);
+  $newArticle.find('time').html(this.publishedOn);
 
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -38,12 +31,10 @@ Article.prototype.toHtml = function() {
 };
 
 rawData.sort(function(a,b) {
-  // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
 rawData.forEach(function(articleObject) {
-  // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
   articles.push(new Article(articleObject));
 });
 
